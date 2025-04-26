7fa421b743696b4a6f69a73fdf7da945b9795bd4
ericgoms <eric@relier.com.br>
2025-04-26 11:55:24 -0300

    Auto-generated commit

---
+++ 8.be50090777de1c85.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcartola_app=self.webpackChunkcartola_app||[]).push([[8],{8:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
